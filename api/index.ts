525c49ac8e52e63edc0772d17ce53a26c8051725
Cao Vy <icaovy2001@gmail.com>
2024-12-19 14:33:35 +0700

    UPDATE:: cors

--- api/index.ts
+++ api/index.ts
@@ -13,11 +13,7 @@ const app = express();
 connectionDB();
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
-    credentials: true,
-  })
+  cors()
 );
 
 app.use(logger("dev"));
